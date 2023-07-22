268cc5a489ad1e8831e4d3f57964309317ddad01
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2023-07-21 22:21:13 -0300

    feat: upload base for the app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import process from 'tailwindcss/lib';
+import styles from './styles.css';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!!</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+const nativewind = require('nativewind/tailwind/native');
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
