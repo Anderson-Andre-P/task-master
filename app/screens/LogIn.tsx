57b2d9dc6559d57ba840b2e3acea47132ccf5e7a
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2023-07-23 21:56:02 -0300

    feat: add validation in forms

--- app/screens/LogIn.tsx
+++ app/screens/LogIn.tsx
@@ -1,17 +1,10 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import React from 'react';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 import { Ionicons } from '@expo/vector-icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
-import { colors, fontSizes, spacing, typography } from '../theme';
+import { colors } from '../theme';
 import AppTextInput from '../components/AppTextInput';
 import { TextTitle } from '../components/Texts/TextTitle';
 import { TextBody } from '../components/Texts/TextBody';
@@ -19,10 +12,56 @@ import { StyleGuide } from '../theme/StyleGuide';
 import { PrimaryButton } from '../components/Buttons/FullButtons/PrimaryButton';
 import { SeccondaryButton } from '../components/Buttons/FullButtons/SeccondaryButton';
 import { CalloutButton } from '../components/Texts/CalloutButton';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import * as LocalAuthentication from 'expo-local-authentication';
+import { TextInput } from 'react-native';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
-const LoginScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
+const LoginScreen = ({ navigation: { navigate } }) => {
+  const [isChecked, setChecked] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const [isBiometricSupported, setIsBiometricSupported] = React.useState(false);
+
+  const ValidateLogin = Yup.object().shape({
+    password: Yup.string()
+      .min(8, 'A sua senha precisa ter pelo menos 8 caracteres!')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        'Devem ser caracteres, pelo menos uma letra, um número e um caractere especial',
+      )
+      .required('Obrigatório'),
+    email: Yup.string().email('E-mail inválido').required('Obrigatório'),
+  });
+
+  const authenticate = async () => {
+    const auth = LocalAuthentication.authenticateAsync({
+      fallbackLabel: 'Inserir senha',
+      cancelLabel: 'Cancelar',
+      requireConfirmation: false,
+    });
+
+    auth.then((result) => {
+      setIsAuthenticated(result.success);
+
+      if (result.success) {
+        navigate('Home');
+      }
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      setIsBiometricSupported(compatible);
+    })();
+  });
+
+  const checked = () => {
+    setChecked(false);
+  };
+
   return (
     <SafeAreaView>
       <View
@@ -46,21 +85,63 @@ const LoginScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
           />
         </View>
 
-        <AppTextInput placeholder="E-mail" />
-        <AppTextInput placeholder="Senha" />
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validateOnMount={true}
+          validationSchema={ValidateLogin}
+          onSubmit={(values) => navigate('Home')}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            touched,
+            values,
+            errors,
+            isValid,
+          }) => (
+            <View>
+              <AppTextInput
+                placeholder="E-mail"
+                value={values.email}
+                onChangeText={handleChange('email')}
+              />
+              <View style={{ marginBottom: 20 }}>
+                {errors.email && touched.email ? (
+                  <Text style={{ color: 'red', fontSize: 15 }}>
+                    {errors.email}
+                  </Text>
+                ) : null}
+              </View>
+              <AppTextInput
+                placeholder="Senha"
+                value={values.password}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+              />
+              <View style={{ marginBottom: 20 }}>
+                {errors.password && touched.password ? (
+                  <Text style={{ color: 'red', fontSize: 15 }}>
+                    {errors.password}
+                  </Text>
+                ) : null}
+              </View>
+              <PrimaryButton text={'Entrar'} onPress={handleSubmit} />
+            </View>
+          )}
+        </Formik>
 
-        <CalloutButton text={'Esqueceu a sua senha?'} />
+        <SeccondaryButton
+          text={'Criar uma nova conta'}
+          onPress={() => navigate('Register')}
+        />
 
         <View
           style={{
             marginTop: StyleGuide.spacing * 3,
           }}
         >
-          <PrimaryButton text={'Entrar'} onPress={() => navigate('Home')} />
-          <SeccondaryButton
-            text={'Criar uma nova conta'}
-            onPress={() => navigate('Register')}
-          />
+          <CalloutButton text={'Esqueceu a sua senha?'} />
         </View>
 
         <View
--- app/screens/Register.tsx
+++ app/screens/Register.tsx
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
@@ -18,10 +18,59 @@ import { PrimaryButton } from '../components/Buttons/FullButtons/PrimaryButton';
 import { SeccondaryButton } from '../components/Buttons/FullButtons/SeccondaryButton';
 import { CalloutButton } from '../components/Texts/CalloutButton';
 import { colors } from '../theme';
+import * as Yup from 'yup';
+import * as LocalAuthentication from 'expo-local-authentication';
+import { Formik } from 'formik';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Register'>;
 
 const RegisterScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
+  const [isChecked, setChecked] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const [isBiometricSupported, setIsBiometricSupported] = React.useState(false);
+
+  const ValidateLogin = Yup.object().shape({
+    password: Yup.string()
+      .min(8, 'A sua senha precisa ter pelo menos 8 caracteres!')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        'Devem ser caracteres, pelo menos uma letra, um número e um caractere especial',
+      )
+      .required('Obrigatório'),
+    password_confirm: Yup.string()
+      .label('Confirmar senha')
+      .required()
+      .oneOf([Yup.ref('password'), null], 'As senhas precisam ser iguais'),
+    email: Yup.string().email('E-mail inválido').required('Obrigatório'),
+  });
+
+  const authenticate = async () => {
+    const auth = LocalAuthentication.authenticateAsync({
+      fallbackLabel: 'Inserir senha',
+      cancelLabel: 'Cancelar',
+      requireConfirmation: false,
+    });
+
+    auth.then((result) => {
+      setIsAuthenticated(result.success);
+
+      if (result.success) {
+        navigate('Home');
+      }
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      setIsBiometricSupported(compatible);
+    })();
+  });
+
+  const checked = () => {
+    setChecked(false);
+  };
+
   return (
     <SafeAreaView>
       <View
@@ -49,16 +98,71 @@ const RegisterScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
             marginVertical: StyleGuide.spacing * 3,
           }}
         >
-          <AppTextInput placeholder="E-mail" />
-          <AppTextInput placeholder="Senha" />
-          <AppTextInput placeholder="Confirmar senha" />
-        </View>
+          <Formik
+            initialValues={{ email: '', password: '', password_confirm: '' }}
+            validateOnMount={true}
+            validationSchema={ValidateLogin}
+            onSubmit={(values) => navigate('Home')}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              touched,
+              values,
+              errors,
+              isValid,
+            }) => (
+              <View>
+                <AppTextInput
+                  placeholder="E-mail"
+                  value={values.email}
+                  onChangeText={handleChange('email')}
+                />
+                <View style={{ marginBottom: 20 }}>
+                  {errors.email && touched.email ? (
+                    <Text style={{ color: 'red', fontSize: 15 }}>
+                      {errors.email}
+                    </Text>
+                  ) : null}
+                </View>
+                <AppTextInput
+                  placeholder="Senha"
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                />
+                <View style={{ marginBottom: 20 }}>
+                  {errors.password && touched.password ? (
+                    <Text style={{ color: 'red', fontSize: 15 }}>
+                      {errors.password}
+                    </Text>
+                  ) : null}
+                </View>
 
-        <PrimaryButton text={'Cadastrar'} onPress={() => {}} />
-        <SeccondaryButton
-          text={'Já possuo uma conta'}
-          onPress={() => navigate('Home')}
-        />
+                <AppTextInput
+                  placeholder="Confirmar senha"
+                  value={values.password_confirm}
+                  onChangeText={handleChange('password_confirm')}
+                  onBlur={handleBlur('password_confirm')}
+                />
+                <View style={{ marginBottom: 20 }}>
+                  {errors.password_confirm && touched.password_confirm ? (
+                    <Text style={{ color: 'red', fontSize: 15 }}>
+                      {errors.password_confirm}
+                    </Text>
+                  ) : null}
+                </View>
+
+                <PrimaryButton text={'Cadastrar'} onPress={handleSubmit} />
+                <SeccondaryButton
+                  text={'Já possuo uma conta'}
+                  onPress={() => navigate('Home')}
+                />
+              </View>
+            )}
+          </Formik>
+        </View>
 
         <View
           style={{
