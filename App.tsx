9578bb4d9ac8e28b5aa8de9c4422d78ff816f164
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2023-07-24 13:07:51 -0300

    fix: add async in fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import { AppNavigator } from './app/navigators/AppNavigator';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
+import { AppNavigator } from "./app/navigators/AppNavigator";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
+import * as Font from "expo-font";
+
+import * as SplashScreen from "expo-splash-screen";
+
+import {
+  Poppins_300Light as poppinsLight,
+  Poppins_400Regular as poppinsRegular,
+  Poppins_500Medium as poppinsMedium,
+  Poppins_600SemiBold as poppinsSemiBold,
+  Poppins_700Bold as poppinsBold,
+} from "@expo-google-fonts/poppins";
+
+import {
+  Roboto_300Light as robotoLight,
+  Roboto_400Regular as robotoRegular,
+  Roboto_500Medium as robotoMedium,
+  Roboto_700Bold as robotoBold,
+} from "@expo-google-fonts/roboto";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          poppinsLight,
+          poppinsBold,
+          poppinsSemiBold,
+          poppinsRegular,
+          poppinsMedium,
+          robotoBold,
+          robotoLight,
+          robotoMedium,
+          robotoRegular,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
       <AppNavigator />
       <StatusBar />
     </SafeAreaProvider>
